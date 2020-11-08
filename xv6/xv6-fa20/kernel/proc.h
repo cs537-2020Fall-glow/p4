f4117195efa6afe12dd62725be488da5e5cacbc7
Kevin Kokesh <kokesh@royal-10.cs.wisc.edu>
2020-11-08 17:20:14 -0600

    P4B: update size across threads

--- xv6/xv6-fa20/kernel/proc.h
+++ xv6/xv6-fa20/kernel/proc.h
@@ -74,6 +74,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  uint stackHigh;              // P4B - Highest address in the stack (for threads)
 };
 
 // Process memory is laid out contiguously, low addresses first:
